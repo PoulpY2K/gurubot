d9fff5a02ad927b897027bcf4a38710cde347964
Jérémy Laurent <contact@jeremy-laurent.com>
2022-11-24 22:27:07 +0100

    build(database): Add mariaDB database and node connector

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,6 @@ import { IntentsBitField } from "discord.js";
 import { Client } from "discordx";
 
 export const bot = new Client({
-  // To use only guild command
-  // botGuilds: [(client) => client.guilds.cache.map((guild) => guild.id)],
-
   // Discord intents
   intents: [
     IntentsBitField.Flags.Guilds,
@@ -18,16 +15,11 @@ export const bot = new Client({
 
   // Debug logs are disabled in silent mode
   silent: false,
-
-  // Configuration for @SimpleCommand
-  simpleCommand: {
-    prefix: "!",
-  },
 });
 
 bot.once("ready", async () => {
   // Make sure all guilds are cached
-  // await bot.guilds.fetch();
+  await bot.guilds.fetch();
 
   // Synchronize applications commands with Discord
   await bot.initApplicationCommands();
@@ -36,15 +28,15 @@ bot.once("ready", async () => {
   // This is useful when moving from guild commands to global commands
   // It must only be executed once
   //
-  //  await bot.clearApplicationCommands(
-  //    ...bot.guilds.cache.map((g) => g.id)
-  //  );
+  await bot.clearApplicationCommands(
+      ...bot.guilds.cache.map((g) => g.id)
+  );
 
   console.log("Bot started");
 });
 
 bot.on("interactionCreate", (interaction: Interaction) => {
-  bot.executeInteraction(interaction);
+  bot.executeInteraction(interaction, true);
 });
 
 bot.on("messageCreate", (message: Message) => {
@@ -52,10 +44,6 @@ bot.on("messageCreate", (message: Message) => {
 });
 
 async function run() {
-  // The following syntax should be used in the commonjs environment
-  //
-  // await importx(__dirname + "/{events,commands}/**/*.{ts,js}");
-
   // The following syntax should be used in the ECMAScript environment
   await importx(`${dirname(import.meta.url)}/{events,commands}/**/*.{ts,js}`);
 
